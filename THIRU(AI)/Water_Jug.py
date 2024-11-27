9d6c73d0fd134075cf044dc5a2edeb492b6f576d
Thirumalesh25 <229x1a0540@gprec.ac.in>
2024-11-27 20:57:11 +0530

    Update Water_Jug.py

--- THIRU(AI)/Water_Jug.py
+++ THIRU(AI)/Water_Jug.py
@@ -3,12 +3,12 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-def pour(m, n, d):
-    j1, j2 = m, 0
+def pour(j1c, j2c, d):
+    j1, j2 = j1c, 0
     steps = [(j1, j2)]
     
     while j1 != d and j2 != d:
-        t = min(j1, n - j2)
+        t = min(j1, j2c - j2)
         j2 += t
         j1 -= t
         steps.append((j1, j2))
@@ -16,28 +16,28 @@ def pour(m, n, d):
         if j1 == d or j2 == d:
             break
         if j1 == 0:
-            j1 = m
+            j1 = j1c
             steps.append((j1, j2))
-        if j2 == n:
+        if j2 == j2c:
             j2 = 0
             steps.append((j1, j2))
     
     return steps
 
-def minSteps(m, n, d):
-    if d > max(m, n) or d % gcd(m, n) != 0:
+def minSteps(j1c, j2c, d):
+    if d > max(j1c, j2c) or d % gcd(j1c, j2c) != 0:
         return []
-    steps1 = pour(m, n, d)
-    steps2 = pour(n, m, d)
+    steps1 = pour(j1c, j2c, d)
+    steps2 = pour(j2c, j1c, d)
     if(len(steps1) <= len(steps2)): print("jug 1, jug 2")
     else: print("jug 2, jug 1")
     return steps1 if len(steps1) <= len(steps2) else steps2
 
-m = int(input("Enter jug 1 capacity: "))
-n = int(input("Enter jug 2 capacity: "))
+j1c = int(input("Enter jug 1 capacity: "))
+j2c = int(input("Enter jug 2 capacity: "))
 d = int(input("Enter target volume: "))
 
-r = minSteps(m, n, d)
+r = minSteps(j1c, j2c, d)
 if r:
     for i in r:
         print(i)
